3bf75b25261a23c48b18fd0424a62de8b5af3f98
King-Emon <tanvirhossainemon835@.com>
2025-05-11 21:47:31 +0600

    Python First Project

---
+++ main-shortened.py
@@ -0,0 +1,40 @@
+# Snake, Water, Gun Game
+import random
+
+computer = random.choice([1, -1, 0])
+playerStr = input("Enter your choice: ")
+playerDist = {"S" : 1, "W" : -1, "G" : 0}
+reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
+player = playerDist[playerStr]
+
+# By now we have 2 numbers (variables) player and computer
+
+print(f"Player chose: {reverseDict[player]}\nComputer chose: {reverseDict[computer]}")
+
+if player == computer:
+  print("It's a tie!")
+else:
+  if(computer - player) == -1 or (computer - player) == 2:
+    print("Player Lose!")
+  else:
+    print("Player Win!")
+
+
+'''
+  if(player == 1 and computer == -1):      # -2
+    print("Player wins!")
+  elif(player == 1 and computer == 0):     # -1
+    print("Player Lose!")
+  elif(player == -1 and computer == 1):    # 2
+    print("Player Lose!")
+  elif(player == -1 and computer == 0):    # 1
+    print("Player Win!")
+  elif(player == 0 and computer == 1):     # 1
+    print("Player Win!")
+  elif(player == 0 and computer == -1):    # -1
+    print("Player Lose!")
+  else:
+    print("Something went wrong!")
+
+'''
+
